0ce13eb66f0e50132b70d0660c832f48d08ef29e
UnaeKang <inpou1@naver.com>
2024-12-03 10:31:09 +0900

    feat: login

---
+++ src/main/java/com/lion/demo/entity/Book.java
@@ -0,0 +1,14 @@
+package com.lion.demo.entity;
+
+import jakarta.persistence.Entity;
+
+
+public class Book {
+    private long bid;
+    private String title;
+    private String author;
+    private String company;
+    private int price;
+    private String imgUrl;
+    private String summary;
+}
